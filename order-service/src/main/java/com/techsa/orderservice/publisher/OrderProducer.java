b4eb78ea0171a455db239685ca80a8465a90b642
Senya- Man <giftmasenya@yahoo.com>
2023-01-29 12:38:07 +0200

    created RabbitMQ Producer

---
+++ order-service/src/main/java/com/techsa/orderservice/publisher/OrderProducer.java
@@ -0,0 +1,32 @@
+package com.techsa.orderservice.publisher;
+
+import com.techsa.orderservice.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderProducer.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.binding.routing.key}")
+    private String orderRoutingKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public OrderProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(OrderEvent orderEvent) {
+        LOGGER.info(String.format("Order event sent to RabbitMQ => %s", orderEvent.toString()));
+        rabbitTemplate.convertAndSend(exchange, orderRoutingKey, orderEvent);
+    }
+
+}
