b4f09bfc1ecab4eb97041799282ed553e92bd7b0
Senya- Man <giftmasenya@yahoo.com>
2023-01-30 12:59:18 +0200

    create RabbitMQ consumer

---
+++ stock-service/src/main/java/com/techsa/stockservice/consumer/OrderConsumer.java
@@ -0,0 +1,21 @@
+package com.techsa.stockservice.consumer;
+
+import com.techsa.stockservice.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @RabbitListener(queues = "${rabbitmq.queue.order.name}")
+    public void consume(OrderEvent event) {
+        LOGGER.info(String.format("Order event received => %s", event.toString()));
+
+        // save order event data in database.
+    }
+
+}
